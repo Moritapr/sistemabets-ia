fba97b82c5bd798d4988b438251a545d4f5e81d5
Moritapr <alejomora2403@gmail.com>
2026-01-27 23:53:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -356,7 +356,7 @@ class RecolectorAutomatico:
 
 
 
-     def entrenar_con_bd(self):
+    def entrenar_con_bd(self):
         partidos = self.db.obtener_todos_partidos()
         
         if len(partidos) < 100:
@@ -1542,6 +1542,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
